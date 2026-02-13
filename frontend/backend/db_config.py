92b29dc1e8ec544429e484862f70c3e3bb530550
Naveen-NK07 <naveenparthipan07@gmail.com>
2026-02-13 11:15:33 +0530

    modified

--- frontend/backend/db_config.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import os
-
-DB_PATH = os.path.join(os.path.dirname(__file__), "database.db")
-
-def get_connection():
-    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
-    conn.row_factory = sqlite3.Row
-    return conn
